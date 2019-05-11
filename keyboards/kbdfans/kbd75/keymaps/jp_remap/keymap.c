a3d848adae2824afee7295028e177a5a370680b2
cnaos <cnaos.gm@gmail.com>
2019-05-11 21:20:31 +0900

    osのキーボード認識をjisのまま、usレイアウトを使う

--- keyboards/kbdfans/kbd75/keymaps/jp_remap/keymap.c
+++ keyboards/kbdfans/kbd75/keymaps/jp_remap/keymap.c
@@ -2,24 +2,35 @@
 #include "keymap_jp.h"
 
 #define _QWERTY   0
-#define _FUNCTION 1
-#define _SHIFT    2
+#define _SHIFT    1
+#define _FUNCTION 2
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
-  FUNCTION,
   SHIFT,
+  FUNCTION,
 };
+
+static bool shift_held;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
-    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  MO(1),    KC_DEL,
-    KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_DEL,   KC_BSPC,  KC_HOME,
-    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGUP,
-    KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                      KC_ENT,   KC_PGDN,
-    KC_LSFT,  MO(1),    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_END,
-    KC_LCTL,  KC_LGUI,  KC_LALT,                      KC_SPC,   KC_SPC,   KC_SPC,                       KC_RALT,  MO(1),    KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
+    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  MO(2),    KC_DEL,
+    KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  JP_EQL,   KC_DEL,   KC_BSPC,  KC_HOME,
+    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     JP_LBRC,  JP_RBRC,  JP_YEN,            KC_PGUP,
+    KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  JP_QUOT,                      KC_ENT,   KC_PGDN,
+    SHIFT,    MO(2),    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_END,
+    KC_LCTL,  KC_LGUI,  KC_LALT,                      KC_SPC,   KC_SPC,   KC_SPC,                       KC_RALT,  MO(2),    KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
+  ),
+
+  [_SHIFT] = LAYOUT(
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  JP_AT,    _______,  _______,  _______,  JP_CIRC,  JP_AMPR,  JP_ASTR,  JP_LPRN,  JP_RPRN,  JP_UNDS,  JP_PLUS,  _______,  RESET,    _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  JP_PIPE,            _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  JP_COLN,  JP_DQT,                       _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,
+    _______,  _______,  _______,                      _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,  _______
   ),
 
   [_FUNCTION] = LAYOUT(
@@ -31,3 +42,58 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,  _______,  _______,                      _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,  _______
   ),
 };
+
+
+void persistent_default_layer_set(uint16_t default_layer) {
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
+}
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+        print("mode just switched to qwerty and this is a huge string\n");
+        set_single_persistent_default_layer(_QWERTY);
+      }
+      return false;
+      break;
+    case SHIFT:
+      if (record->event.pressed) {
+        register_code(KC_LSFT);
+        layer_on(_SHIFT);
+        shift_held = true;
+      } else {
+        shift_held = false;
+        layer_off(_SHIFT);
+        unregister_code(KC_LSFT);
+      }
+      return false;
+      break;
+    case FUNCTION:
+      if (record->event.pressed) {
+        layer_on(_FUNCTION);
+      } else {
+        layer_off(_FUNCTION);
+      }
+      return false;
+      break;
+
+    case JP_AT:
+    case JP_COLN:
+    case JP_CIRC:
+      if (record->event.pressed) {
+        unregister_code(KC_LSFT);
+        register_code(keycode);
+      } else {
+        if (shift_held){
+          register_code(KC_LSFT);
+        }
+        unregister_code(keycode);
+      }
+      return true;
+
+  }
+  return true;
+}
